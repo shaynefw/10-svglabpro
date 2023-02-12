98b22cea96401157dcfae394cb5662c85dbc2ed4
swmac <shaynefw@gmail.com>
2023-02-12 14:48:26 -0500

    added features, files,  and comments

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const CLI = require('./lib/CLI');
+const GenerateLogo = require('./lib/generateLogo');
 
-inquirer.prompt([
-    {
-        name: 'key',
-        type: 'press-to-continue',
-        message: 'Welcome to SVG Labs Pro press any key to start creating your logo...'
-    },
-    {
-        name: 'input',
-        message: 'What name or initials would you like to be on the logo? (maximum 3 characters)',
-        validate: (input) => input.length <= 3 ? true : 'Please enter up to 3 characters.'
-    }
-])
-.then((response) => {
-    console.log('File creation test!');
+// Calling the promptUser method of the CLI module, which returns a Promise
+CLI.promptUser()
+// Once the Promise is resolved, the then() method is called with the resolved value (answers) as its argument
+.then((answers) => {
+  // Calling the generate method of the GenerateLogo module, passing in the resolved answers from the CLI prompt
+  GenerateLogo.generate(answers.text, answers.textColor, answers.shape, answers.shapeColor);
 });
